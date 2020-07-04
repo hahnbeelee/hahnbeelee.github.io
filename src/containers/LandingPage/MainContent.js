d5378a356ed02489fe3490ed43ce223ffcba0ea6
Hahnbee Lee <hl985@cornell.edu>
2020-07-03 23:32:31 -0400

    json created

--- src/containers/LandingPage/MainContent.js
+++ src/containers/LandingPage/MainContent.js
@@ -1,91 +1,71 @@
-import React from 'react';
+import React from 'react'
 
-import { Tabs, Row, Space } from 'antd';
-import MyCard from '../../components/MyCard';
-const { TabPane } = Tabs;
+import { Tabs, Row, Checkbox } from 'antd'
+import MyCard from '../../components/MyCard'
+import content from '../../data/content.json'
 
+const { TabPane } = Tabs
 
-class MainContent extends React.Component {
-
-    render () {
-        return (
-            <div id="main-content">
-                <Tabs>
-                    <TabPane tab="Experience" key="1">
-                        {/* add link later when page is created */}
-                        {/* include react projects! */}
-
-                        <Row>
-                            <MyCard 
-                                title="Leidos"
-                                description="UI Development Intern"
-                            />
-                            <MyCard 
-                                title="Scriyb LLC"
-                                description="Algorithm Developer"
-                            />
-                            <MyCard 
-                                title="Vera.Zone"
-                                description="Front-End Developer"
-                            />
-                        </Row>
-                    </TabPane>
-                    <TabPane tab="Projects" key="2">
-                        <Row>
-                            <MyCard 
-                                title="Cornell Rocketry Team"
-                                description="Engineering Project Team"
-                            />
-                            <MyCard 
-                                title="Medium Design Collective"
-                                description="Restroom Signage Redesign"
-                            />
-                            <MyCard 
-                                title="PWC Mutual Aid Group"
-                                description="Web Developer"
-                            />
-                            <MyCard 
-                                title="React Components"
-                                description="Web Developer"
-                            />
-                            <MyCard 
-                                title="Cornell Eastern Music Ensemble"
-                                description="Webmaster"
-                            />
-                        </Row>
-                    </TabPane>
-                    <TabPane tab="Classes" key="3">
-                        {/* most relevant */}
-                        <Row>
-                            <MyCard 
-                                title="Intro Design & Prog for Web"
-                                description="Skills: HTML, CSS, javascript, jQuery"
-                            />
-                            <MyCard 
-                                title="Intmd. Design & Prog for Web"
-                                description="Skills: HTML, CSS,PHP, MySQL"
-                            />
-                            <MyCard 
-                                title="Intro to Data Science"
-                                description="Skills: jupyter notebooks,......"
-                            /> {/* final project, psets = password protected */}
-                            <MyCard 
-                                title="Functional Programming"
-                                description="Language: OCaml"
-                            /> {/* final project, psets = pw protected */}
-                            <MyCard 
-                                title="Obj-Oriented Programming"
-                                description="Language: java"
-                            /> {/* psets = pw protected */}
-                        </ Row>
-                    </TabPane>
-                    <TabPane tab="All" key="4">
-                        Content of Tab 4
-                    </TabPane>
-                </Tabs>
-            </div>
-        );
+// e.target.checked
+const MainContent = () => {
+    const createCards = (data) => {
+        let cards = []
+        for(var i=0; i<data.length; i++){
+            cards.push(
+                <MyCard 
+                    title= {data[i].title}
+                    description={data[i].description}
+                />
+            )
+        }
+        return cards
     }
-};
+    return (
+        <div id="main-content">
+            <Tabs>
+                <TabPane tab="Experience" key="1">
+                    {/* add link later when page is created */}
+                    {/* include react projects! */}
+
+                    <Row>
+                        {createCards(content.experiences)}
+                    </Row>
+                </TabPane>
+                <TabPane tab="Projects" key="2">
+                    <Row>
+                        {createCards(content.projects)}
+                    </Row>
+                </TabPane>
+                <TabPane tab="Classes" key="3">
+                    {/* most relevant */}
+                    <Checkbox>Relevance</Checkbox>
+                    <Row>
+                        {createCards(content.classes)}
+                    </ Row>
+                </TabPane>
+                <TabPane tab="All" key="4">
+                    <Row>
+                        {createCards(content.experiences)}
+                        {createCards(content.projects)}
+                        {createCards(content.classes)}
+                    </Row>
+                </TabPane>
+            </Tabs>
+        </div>
+    )
+}
 
 export default MainContent
+
+// <MyCard 
+//     title="Intro to Data Science"
+//     description="Skills: jupyter notebooks,......"
+// /> {/* final project, psets = password protected */}
+// <MyCard 
+//     title="Functional Programming"
+//     description="Language: OCaml"
+// /> {/* final project, psets = pw protected */}
+// <MyCard 
+//     title="Obj-Oriented Programming"
+//     description="Language: java"
+// /> {/* psets = pw protected */}
