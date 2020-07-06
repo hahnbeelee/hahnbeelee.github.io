704e246994c3bbe050923a8f241cab3f3979612d
Hahnbee Lee <hl985@cornell.edu>
2020-07-06 00:17:25 -0400

    filter classes!

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -4,24 +4,20 @@ import headshot from '../../images/headshot.png';
 
 import MainContent from './MainContent';
 
-class LandingPage extends React.PureComponent {
-    render() {
-        return (
-            <div>
-                <div class="parent uk-flex uk-padding-large uk-flex-center uk-grid-match uk-margin-auto" uk-height-match>
-                    <div class="text uk-text-center uk-padding">
-                        <p>
-                            I am Hahnbee Lee. I am an undergraduate at Cornell University double majoring in 
-                            Information Science & Computer Science and minoring in Law & Society. I am a
-                            creative as well as a technical person.
-                        </p>
-                    </div>
-                    <img class="uk-align-right@m uk-margin-remove-adjacent headshot" src={headshot} alt="Hahnbee's headshot"></img>
-                </div> 
-                <MainContent />
+const LandingPage = () => (
+    <div>
+        <div class="parent uk-flex uk-padding-large uk-flex-center uk-grid-match uk-margin-auto" uk-height-match>
+            <div class="text uk-text-center uk-padding">
+                <p>
+                    I am Hahnbee Lee. I am an undergraduate at Cornell University double majoring in 
+                    Information Science & Computer Science and minoring in Law & Society. I am a
+                    creative as well as a technical person.
+                </p>
             </div>
-        );
-    }
-}
+            <img class="uk-align-right@m uk-margin-remove-adjacent headshot" src={headshot} alt="Hahnbee's headshot"></img>
+        </div> 
+        <MainContent />
+    </div>
+)
 
 export default LandingPage;
--- src/containers/LandingPage/MainContent.js
+++ src/containers/LandingPage/MainContent.js
@@ -1,54 +1,134 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { Tabs, Row, Checkbox } from 'antd'
+import { Tabs, Row, Typography , Radio, Divider } from 'antd'
 import MyCard from '../../components/MyCard'
 import content from '../../data/content.json'
 
-const { TabPane } = Tabs
 
 // e.target.checked
 const MainContent = () => {
-    const createCards = (data) => {
+    //components
+    const { TabPane } = Tabs
+    const { Title } = Typography
+
+    //data
+    const filters = ['Category', 'Alphabetical'] //, 'Amount of Content', 'Chronological'
+
+    //state
+    const [value, setValue] = useState('Category')
+
+    //functions
+
+    const onChange = e => {
+        setValue(e.target.value)
+    }
+
+    const createCards = (group, isType) => {
         let cards = []
-        for(var i=0; i<data.length; i++){
-            cards.push(
-                <MyCard 
-                    title= {data[i].title}
-                    description={data[i].description}
-                />
+        const dataType = isType ? 'type' : 'category'
+        for(let i=0; i<content.length; i++){
+            if(content[i][dataType] === group){
+                cards.push(
+                    <MyCard 
+                        title= {content[i].title}
+                        description={content[i].description}
+                    />
+                )
+            }
+        }
+         
+        return(
+           <Row>
+               {cards}
+           </Row> 
+        ) 
+    }
+
+    const createCategories = () => {
+        const categories = ['Coding', 'Data Science', 'Information Science', 'Law']
+        let formattedCats = categories.map((category) =>
+            <div>
+                <Divider />
+                <Title level={3}>{category}</Title>
+                {createCards(category, false)}
+            </div>
+        );
+        formattedCats[0] = (
+            <div>
+                <Title level={3}>{categories[0]}</Title>
+                {createCards(categories[0], false)}
+            </div>
+        )
+        return (
+            <div>
+                {formattedCats}
+            </div>
+        )
+    }
+
+    const createClasses = (filter) => {
+        if(filter === 'Category'){
+           return createCategories()
+        } else if(filter === 'Alphabetical'){
+            // get all classes
+            let classes = []
+            for(let i=0; i<content.length; i++){
+                if(content[i].type === 'Classes'){
+                    classes.push(content[i])
+                }
+            }
+            classes.sort((a, b) => a.title.localeCompare(b.title)) //sort alphabetically
+            // format
+            let formatted = []
+            for(let x of classes){
+                formatted.push(
+                    <MyCard 
+                        title= {x.title}
+                        description={x.description}
+                    />
+                )
+            }
+            return (
+                <Row>
+                    {formatted}
+                </Row>
             )
         }
-        return cards
     }
+
+
+    
     return (
         <div id="main-content">
             <Tabs>
+
                 <TabPane tab="Experience" key="1">
                     {/* add link later when page is created */}
                     {/* include react projects! */}
+                    {createCards('Experience', true)}
+                </TabPane>
 
-                    <Row>
-                        {createCards(content.experiences)}
-                    </Row>
-                </TabPane>
                 <TabPane tab="Projects" key="2">
-                    <Row>
-                        {createCards(content.projects)}
-                    </Row>
+                    {createCards('Projects', true)}
                 </TabPane>
+
                 <TabPane tab="Classes" key="3">
-                    {/* most relevant */}
-                    <Checkbox>Relevance</Checkbox>
-                    <Row>
-                        {createCards(content.classes)}
-                    </ Row>
+
+                    <p>Filter By:</p>
+                    <Radio.Group 
+                        options={filters} 
+                        onChange={onChange}
+                        value={value}
+                    />
+                    <Divider />
+                    {createClasses(value)}
+                    
                 </TabPane>
+
                 <TabPane tab="All" key="4">
-                    <Row>
-                        {createCards(content.experiences)}
-                        {createCards(content.projects)}
-                        {createCards(content.classes)}
-                    </Row>
+                    {createCards('Experience', true)}
+                    {createCards('Projects', true)}
+                    {createCards('Classes', true)}
                 </TabPane>
             </Tabs>
         </div>
