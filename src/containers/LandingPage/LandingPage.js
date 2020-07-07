7836fa5ded3a3bc7f152bada223bbd53163b43ba
Hahnbee Lee <hl985@cornell.edu>
2020-07-07 16:54:15 -0400

    highlight

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -3,14 +3,24 @@ import { Link } from 'react-router-dom'
 
 import MainContent from './MainContent'
 
+import highlight from '../../images/highlight.svg'
+
 const LandingPage = () => (
     <main id="landing">
-        <p id="name">
-            My name is <Link to='/about'>Hahnbee</Link>
-        </p>
-        <p id="me">
-            I am passionate about creating elegant code and designs.
-        </p>
+        <div id="banner">
+            <p id="name">
+                My name is 
+                <div id="hahnbee">
+                    <img src={highlight} alt="highlight" />
+                    <Link to='/about' id="link">
+                        Hahnbee
+                    </Link>
+                </div>
+            </p>
+            <p id="me">
+                I am passionate about creating elegant code and designs.
+            </p>
+        </div>
         <MainContent />
     </main>
 )
