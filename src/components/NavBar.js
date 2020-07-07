f41322bef46dfeb9e9e436e6dd721967951289e9
Hahnbee Lee <hl985@cornell.edu>
2020-07-07 17:40:23 -0400

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-class NavBar extends React.Component {
-    render() {
-        return(
-            <nav className="uk-navbar">
-                <div className="uk-navbar-center">
-                    <ul className="uk-navbar-nav">
-                        <li>
-                            <Link to="/">home</Link>
-                        </li>
-                        <li>
-                            <Link to="/about">about</Link>
-                        </li>
-                        <li>
-                            <Link to="/work">my work</Link>
-                        </li>
-                        <li>
-                            <Link to="/skills">skills</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        );
-    }
+const NavBar = () => {
+    return(
+        <nav>
+            <ul className="uk-navbar-nav">
+                <li id="home">
+                    <Link to="/" id="a" className="nav">hahnbee lee</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="nav">about</Link>
+                </li>
+                <li>
+                    <Link to="/work" className="nav">my work</Link>
+                </li>
+                <li>
+                    <Link to="/skills" className="nav">skills</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar
