6f27866f7a75746f6c2602b21166ac7ae58d561d
hahnbeelee <hl985@cornell.edu>
2020-08-27 23:09:30 -0400

    goodstuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { Helmet } from "react-helmet";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 
-import LandingPage from './containers/LandingPage/LandingPage';
-import About from './containers/About'
-import Skills from './containers/Skills'
-import Work from './containers/Work'
+import LandingPage from "./containers/LandingPage/LandingPage";
+import About from "./containers/About";
+import Skills from "./containers/Skills";
+import Work from "./containers/Work";
 
 // articles
-import Leidos from './containers/articles/Leidos'
-import VeraZone from './containers/articles/VeraZone'
-import ReactComponents from './containers/articles/ReactComponents'
-import history from './history';
+import Leidos from "./containers/articles/Leidos";
+import VeraZone from "./containers/articles/VeraZone";
+import ReactComponents from "./containers/articles/ReactComponents";
+import GoodStuff from "./containers/articles/GoodStuff";
+import history from "./history";
 
-import './styles/main.scss';
+import "./styles/main.scss";
 
 const App = () => (
   <div>
-    <Helmet
-      titleTemplate="%s - Hahnbee Lee"
-      defaultTitle="Hahnbee Lee"
-    >
+    <Helmet titleTemplate="%s - Hahnbee Lee" defaultTitle="Hahnbee Lee">
       <meta name="description" content="Hahnbee Lee's portfolio" />
     </Helmet>
     <BrowserRouter history={history}>
@@ -33,15 +31,16 @@ const App = () => (
           <Route path="/about" component={About} />
           <Route path="/skills" component={Skills} />
           <Route path="/work" component={Work} />
-          
+
           {/* articles */}
           <Route path="/leidos" component={Leidos} />
           <Route path="/vera-zone" component={VeraZone} />
           <Route path="/react-components" component={ReactComponents} />
+          <Route path="/goodstuff" component={GoodStuff} />
         </Switch>
       </div>
     </BrowserRouter>
   </div>
-  );
+);
 
 export default App;
---
+++ src/containers/articles/GoodStuff.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const GoodStuff = () => <main id="goodstuff">GoodStuff</main>;
+
+export default GoodStuff;
